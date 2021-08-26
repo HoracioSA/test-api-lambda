331f82c075479e0448959d225f3e221f8a7ca252
HoracioSA <hraciosapato2015hcs@gmail.com>
2021-08-26 21:58:35 +0300

    Action 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res, next) => {
     return res.status(200).json({
-      message: "Hello World!",
+      message: "Hello Mundo!",
     });
   });
 module.exports= app
