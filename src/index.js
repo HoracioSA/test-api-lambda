9b0f72a0de63aa4f0a830f11f5ba5029302ba5bc
HoracioSA <hraciosapato2015hcs@gmail.com>
2021-08-27 11:56:28 +0300

    Updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res, next) => {
     return res.status(200).json({
-      message: "Hello Mundo!",
+      message: "Hello World!",
     });
   });
 module.exports= app
